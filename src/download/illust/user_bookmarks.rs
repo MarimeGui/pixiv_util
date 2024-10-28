19d4347116962bcbf9fcc995b5a574cea5072900
Marime Gui <lepro.guillaume@gmail.com>
2024-10-28 12:03:34 +0100

    Blazing fast user bookmarks

--- src/download/illust/user_bookmarks.rs
+++ src/download/illust/user_bookmarks.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::Arc};
 
 use anyhow::Result;
 use tokio::task::JoinSet;
@@ -15,46 +15,77 @@ pub async fn dl_user_bookmarks(
     file_list: Option<Vec<String>>,
     user_id: u64,
 ) -> Result<()> {
+    // Arc for file list to prevent useless copies
+    let file_list = Arc::new(file_list);
+
+    // First DL to get total number of illusts
+    let nb_illusts = dl_one_bookmark_page(
+        client.clone(),
+        dest_dir.clone(),
+        directory_policy,
+        file_list.clone(),
+        user_id,
+        0,
+    )
+    .await?;
+
+    // Assume page will always contain max except for last one, calc number of pages
+    let page_count =
+        nb_illusts / ILLUSTS_PER_PAGE + usize::from(nb_illusts % ILLUSTS_PER_PAGE != 0);
+
+    // Start all pages
+    let mut set = JoinSet::new();
+    for page_id in 1..page_count {
+        set.spawn(dl_one_bookmark_page(
+            client.clone(),
+            dest_dir.clone(),
+            directory_policy,
+            file_list.clone(),
+            user_id,
+            page_id * ILLUSTS_PER_PAGE,
+        ));
+    }
+
+    while let Some(r) = set.join_next().await {
+        r??;
+    }
+
+    Ok(())
+}
+
+async fn dl_one_bookmark_page(
+    client: SemaphoredClient,
+    dest_dir: PathBuf,
+    directory_policy: DirectoryPolicy,
+    file_list: Arc<Option<Vec<String>>>,
+    user_id: u64,
+    offset: usize,
+) -> Result<usize> {
     let mut set = JoinSet::new();
 
-    let mut processed = 0;
+    let body =
+        crate::api_calls::user_bookmarks::get(client.clone(), user_id, offset, ILLUSTS_PER_PAGE)
+            .await?;
 
-    loop {
-        let body = crate::api_calls::user_bookmarks::get(
-            client.clone(),
-            user_id,
-            processed,
-            ILLUSTS_PER_PAGE,
-        )
-        .await?;
-
-        for work in &body.works {
-            // Check if file already downloaded
-            if let Some(files) = &file_list {
-                if is_illust_in_files(&work.id.to_string(), files) {
-                    continue;
-                }
+    for work in &body.works {
+        // Check if file already downloaded
+        if let Some(files) = &*file_list {
+            if is_illust_in_files(&work.id.to_string(), files) {
+                continue;
             }
-
-            set.spawn(dl_one_illust(
-                client.clone(),
-                work.id,
-                dest_dir.clone(),
-                directory_policy,
-            ));
         }
 
-        processed += body.works.len();
-
-        // Got every illust
-        if processed >= body.total {
-            break;
-        }
+        set.spawn(dl_one_illust(
+            client.clone(),
+            work.id,
+            dest_dir.clone(),
+            directory_policy,
+        ));
     }
 
     while let Some(r) = set.join_next().await {
         r??
     }
 
-    Ok(())
+    Ok(body.total)
 }
